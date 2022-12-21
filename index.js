9bf596a2cab05fb403c5649523d0ec531a3db661
Pierce <pierceevans@me.com>
2022-12-20 18:38:05 -0600

    commit tree fab02fdea5e680405b75a48a58887f1cdc51ec5e

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+    return(cats);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(name){
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat(name){
+    return cats.shift();
+}
+function appendCat(name){
+    return [...cats, "Broom"];
+}
+function prependCat(name){
+    return ["Arnold", ...cats];
+}
+function removeLastCat(name){
+    return cats.slice(0,-1);
+}
+function removeFirstCat(name){
+    return cats.slice(1);
+}
